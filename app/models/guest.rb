67adcc36f1876ee51e0e0e7229762661762852c2
Ryan Flynn <ryanflynn44@gmail.com>
2020-07-18 13:57:24 -0400

    Finished AirBnB challenge.

---
+++ app/models/guest.rb
@@ -0,0 +1,30 @@
+class Guest
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self 
+    end
+
+    def self.all 
+        @@all 
+    end 
+
+    def trips
+        Trip.all.select {|trip| trip.guest == self}
+    end
+
+    def listings
+        trips.map {|trip| trip.listing}
+    end
+
+    def self.pro_traveller
+        self.all.select {|guest| guest.trips.count > 1}
+    end 
+
+    def self.find_all_by_name(name)
+        self.all.select {|guest| guest.name == name}
+    end 
+end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,35 @@
 require_relative '../config/environment.rb'
+require_relative '../app/models/guest.rb'
+require_relative '../app/models/listing.rb'
+require_relative '../app/models/trip.rb'
 
 def reload
   load 'config/environment.rb'
 end
+
+#objects to test on
+ryan_f = Guest.new("Ryan")
+melissa = Guest.new("Melissa")
+ryan_w = Guest.new("Ryan")
+seattle = Listing.new("Seattle")
+seattle2 = Listing.new("Seattle")
+miami = Listing.new("Miami")
+seattle_trip = Trip.new(seattle, ryan_f)
+miami_trip = Trip.new(miami, melissa)
+melissa_seattle_trip = Trip.new(seattle, melissa)
+
+
+
+#tests I've done
+ryan_f.trips
+ryan_f.listings
+Guest.pro_traveller
+Guest.find_all_by_name("Ryan")
+seattle.trips 
+seattle.guests 
+seattle.trip_count
+Listing.find_all_by_city("Seattle")
+Listing.most_popular 
+Listing.find_all_by_city("Miami")
 
 Pry.start
