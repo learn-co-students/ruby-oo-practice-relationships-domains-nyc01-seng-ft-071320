b6d7fbb7ca893089f39fa8cc5e845f5bb057c040
junko911 <junkotahara911@gmail.com>
2020-07-20 13:00:29 -0400

    Created actors, actor_count, and self.most_actors methods

--- app/models/imdb/movie.rb
+++ app/models/imdb/movie.rb
@@ -4,12 +4,14 @@ class Movie
   end
 
   def actors
+    characters.map(&:actor)
   end
 
   def actor_count
+    actors.count
   end
 
   def self.most_actors
-
+    all.group_by { |movie| movie.actor_count }.max.last
   end
 end
