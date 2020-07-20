4ddcfd21e7536499f7d328e6eb7aa702fb082d6d
junko911 <junkotahara911@gmail.com>
2020-07-20 12:56:54 -0400

    Created characters method

--- app/models/imdb/movie.rb
+++ app/models/imdb/movie.rb
@@ -1,2 +1,15 @@
 class Movie
+  def characters
+    Character.all.select do |character| character.movie == self }
+  end
+
+  def actors
+  end
+
+  def actor_count
+  end
+
+  def self.most_actors
+
+  end
 end
