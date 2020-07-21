ea940845dfe456933f89527d3b9cf8e641095d90
junko911 <junkotahara911@gmail.com>
2020-07-20 21:58:32 -0400

    Created reader method for name variable.

--- app/models/imdb/show.rb
+++ app/models/imdb/show.rb
@@ -1,4 +1,5 @@
 class Show
+  attr_reader :name
   @@all = []
 
   def initialize(name)
@@ -9,7 +10,7 @@ class Show
   def self.all
     @@all
   end
-  
+
   def on_the_big_screen
     Movie.all.select { |movie| movie.name == self.name }
   end
